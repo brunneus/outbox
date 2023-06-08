054ab81e578f91901394e89ce60a5e1282e611e1
Bruno Joaquim <bruno.joaquim@eximia.co>
2023-06-08 13:37:43 -0300

    Add OpenTelemetry

--- Outbox/Program.cs
+++ Outbox/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Outbox;
 using Outbox.Application;
 using Outbox.Data;
 using Outbox.Entities;
 using Outbox.Services;
+using System.Diagnostics.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,9 +20,44 @@ builder.Services.AddScoped<IBrokerPublisherService, KafkaOrderMessageProducer>()
 builder.Services.AddHostedService<DatabaseMigrationHostedService>();
 builder.Services.AddHostedService<OutboxPublisherBackgroundService>();
 
+var resourceBuilder = ResourceBuilder.CreateDefault().AddService("Outbox");
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracingBuilder =>
+    {
+        tracingBuilder
+            .AddAspNetCoreInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation(e =>
+            {
+                e.EnrichWithIDbCommand = (activity, command) =>
+                {
+                    activity.AddTag("sqlCmd", command.CommandText);
+                };
+            })
+            .AddOtlpExporter()
+            .SetResourceBuilder(resourceBuilder);
+    })
+    .WithMetrics(metricsBuilder =>
+    {
+        metricsBuilder
+            .AddMeter("outbox")
+            .AddOtlpExporter()
+            .SetResourceBuilder(resourceBuilder)
+            .AddAspNetCoreInstrumentation();
+    });
+
 builder.Services.AddDbContext<OutboxDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("Database"));
+});
+
+builder.Logging.ClearProviders();
+
+builder.Logging.AddOpenTelemetry(loggerBuilder =>
+{
+    loggerBuilder
+        .SetResourceBuilder(resourceBuilder)
+        .AddOtlpExporter();
 });
 
 var app = builder.Build();
@@ -30,16 +70,22 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapPost("/order", async ([FromBody]OrderRequest request, [FromServices]OrderService orderService) =>
+app.MapPost("/order", async ([FromBody] OrderRequest request, [FromServices] OrderService orderService, [FromServices]ILogger<Program> logger) =>
 {
     var order = new Order();
 
-    foreach(var item in request.Items)
+    foreach (var item in request.Items)
     {
         order.AddItem(new OrderItem(item.ProductName, item.UnitPrice, item.Discount, item.Units));
     }
 
     await orderService.AddOrderAsync(order);
+
+    logger.LogInformation("Order with Id {OrderId} created", order.Id);
+
+    var meter = new Meter(name: "outbox");
+    var counter = meter.CreateCounter<int>("orders.counting", "Number of orders created");
+    counter.Add(1);
 
     return order;
 })
